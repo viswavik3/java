ad5a48f75ea9c15e158b3429ec9a6cab5cdf16d6
viswavik3 <123868576+viswavik3@users.noreply.github.com>
2023-02-01 18:46:14 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld
 {
   public static void main(String[] args)
   {
-    System.out.println("Hello World, this is Jenkins!!!");
+    System.out.println("Hello World, this is auto build");
   }
 }
